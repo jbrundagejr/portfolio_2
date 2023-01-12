9cc985fcc308f9070d7d466e091426d7bc565946
Jon Brundage Jr <jbrundagejr@gmail.com>
2023-01-11 19:57:43 -0500

    fonts moved

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   css: [
     "~/assets/styles/fonts.css",
-    "~/assets/styles/mobile.css",
+    "~/assets/styles/main.css",
     "~/assets/styles/tablet.css",
     "~/assets/styles/desktop.css",
   ],
